2ac7e2f57d9b36934116c77d1fe749ee0b6d3c9f
PeterTikCheung <petermtcheung@gmail.com>
2024-05-17 02:15:29 +0800

    add user route

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+// Middleware setup
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const userRoutes = require('./routes/user');
+
+app.use('/', homeRoutes);
+app.use('/users', userRoutes);
+
+// Start the server
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ src/routes/user.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const UserController = require('../controllers/UserController');
+
+// GET /users - Get all users
+//router.get('/', UserController.getAllUsers);
+
+// POST /users - Create a new user
+router.post('/', UserController.createUser);
+
+// GET /users/:id - Get a specific user by ID
+//router.get('/:id', UserController.getUserById);
+
+// PUT /users/:id - Update a specific user by ID
+//router.put('/:id', UserController.updateUserById);
+
+// DELETE /users/:id - Delete a specific user by ID
+//router.delete('/:id', UserController.deleteUserById);
+
+module.exports = router;
