ff8b415085cdbb1e5f310b4eb3701e1cf8e26196
PeterTikCheung <petermtcheung@gmail.com>
2024-05-16 14:30:13 +0800

    init product contract

--- contracts/marketplace/Product.sol
+++ contracts/marketplace/Product.sol
@@ -23,6 +23,19 @@ contract Product {
         sellerUuid = _sellerUuid;
     }
 
+    function purchase(uint8 _quantity, string memory _buyerUuid) external payable {
+        require(_quantity > 0, "Quantity must be greater than zero");
+        require(_quantity <= quantity, "Insufficient quantity available");
+        require(msg.value == price * _quantity, "Incorrect payment amount");
 
+        // Perform the purchase
+        quantity -= _quantity;
+
+        // Transfer payment to the seller
+        payable(seller).transfer(msg.value);
+
+        // Emit the purchase event
+        emit ProductPurchased(msg.sender, _buyerUuid, _quantity);
+    }
 
 }
